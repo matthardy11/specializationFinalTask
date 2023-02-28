120450e7a1d1ed8dd7303d7c5c25295889f70a40
matthardy11 <matthardy2560@gmail.com>
2023-02-28 21:07:53 +0400

    add PrintArray method

--- finalProject/Program.cs
+++ finalProject/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу которая из имеющегося массива строк формирует массив из строк,
+// длинна которых меньше либо равна 3 символа
+
+void PrintArray(string[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+  {System.Console.Write(array[i] + ", ");}
+}
+
+
+
